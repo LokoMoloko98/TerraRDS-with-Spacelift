cbe8ef92bcac6fcf35f584f241d0e74e76baa5cb
LokoMoloko98 <communication@moloko-mokubedi.co.za>
2024-08-04 22:36:27 +0200

    postgres14 db_parameter_group

--- main.tf
+++ main.tf
@@ -8,6 +8,16 @@ module "networking" {
   db_connection_port     = 5432
 }
 
+resource "aws_db_parameter_group" "postgres14" {
+  name   = "postgres14"
+  family = "postgres14"
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+}
+
 module "rds-db" {
   source               = "./db"
   security_group_id    = module.networking.security_group_id
